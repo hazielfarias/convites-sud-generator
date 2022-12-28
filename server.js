87c286e0e27427e3cf49b6dffdc39d7131ea07e9
Haziel <hlf@cin.ufpe.br>
2022-12-28 14:39:22 -0300

    Heroku config

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express = require("express");
-const http = require("http");
-const path = require("path");
+//Install express server
+const express = require('express');
+const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, "dist/convites-sud-generator"))); //aqui você define onde está o index.html da sua aplicação.
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/convites-sud-generator/index.html"));
-});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/convites-sud-generator'));
 
-const port = process.env.PORT || 8080;
-app.set("port", port);
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/convites-sud-generator/'}),
+);
 
-const server = http.createServer(app);
-server.listen(port, () => console.log("Running"));
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
